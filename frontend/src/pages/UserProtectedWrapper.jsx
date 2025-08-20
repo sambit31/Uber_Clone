7a25ab1e6c861a05191cb568b75bb506ce0ba46b
Sambit Ghosh <sambitg31@gmail.com>
2025-08-21 00:17:33 +0530

    updated userprotectedWrapper

--- frontend/src/pages/UserProtectedWrapper.jsx
+++ frontend/src/pages/UserProtectedWrapper.jsx
@@ -1,6 +1,5 @@
-import { useNavigate } from 'react-router-dom'; // ✅ import navigate
-import { useEffect } from 'react';
-
+import { useNavigate } from 'react-router-dom'; 
+import { useEffect } from 'react';// ✅ import navigate
 const UserProtectedWrapper = ({ children }) => {
   const token = localStorage.getItem('token');
   const navigate = useNavigate();
